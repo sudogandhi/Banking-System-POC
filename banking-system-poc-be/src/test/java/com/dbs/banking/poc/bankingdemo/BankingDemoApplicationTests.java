78a1af36ab7227b95e0d24bda1bf4dff6083fbfb
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-08-26 23:48:54 -0700

    Backend project in spring and maven is initiated.

---
+++ banking-system-poc-be/src/test/java/com/dbs/banking/poc/bankingdemo/BankingDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dbs.banking.poc.bankingdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankingDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
