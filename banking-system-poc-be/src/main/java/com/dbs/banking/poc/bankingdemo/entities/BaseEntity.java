3fafb1683a1cd4182628d04d91ad76cf82599262
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-08-29 01:40:40 -0700

    Base Entity is created.

---
+++ banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/entities/BaseEntity.java
@@ -0,0 +1,42 @@
+package com.dbs.banking.poc.bankingdemo.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@MappedSuperclass
+@EqualsAndHashCode(of = {"id"})
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    @JsonProperty(value = "id")
+    private Long id;
+
+    @Version
+    @Column(name = "version")
+    @JsonIgnore
+    private Long version;
+
+    @Column(name = "created_at")
+    @JsonIgnore
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    @CreatedDate
+    private DateTime createdAt;
+
+    @Column(name = "updated_at")
+    @JsonIgnore
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    @LastModifiedDate
+    private DateTime updatedAt;
+}
