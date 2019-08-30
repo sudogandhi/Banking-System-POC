fb7f31634d53fc87958f2f3e1f43c39ba5828725
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-08-29 21:09:47 -0700

    Some changes are made.

--- banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/BankingDemoApplication.java
+++ banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/BankingDemoApplication.java
@@ -1,13 +1,14 @@
 package com.dbs.banking.poc.bankingdemo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 public class BankingDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BankingDemoApplication.class, args);
+        run(BankingDemoApplication.class, args);
     }
 
 }
