5e92b977f80293130239ed11522bf06557c7a124
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-08-29 21:03:21 -0700

    Constructor and Images set is added to the User class

--- banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/entities/User.java
+++ banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/entities/User.java
@@ -2,11 +2,15 @@ package com.dbs.banking.poc.bankingdemo.entities;
 
 import com.sun.istack.internal.NotNull;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.joda.time.DateTime;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Data
 @MappedSuperclass
+@EqualsAndHashCode(of = "id")
 public class User extends BaseEntity {
 
     @Column(name = "username")
@@ -35,4 +39,26 @@ public class User extends BaseEntity {
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private Set<Image> images;
+
+    public User(String username, String firstName, String lastName, String email, String password, Long mobileNo, Address address, Set<Image> images) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.mobileNo = mobileNo;
+        this.address = address;
+        this.images = images;
+    }
+
+    public User(Long version, DateTime createdAt, DateTime updatedAt) {
+        super(version, createdAt, updatedAt);
+    }
+
+    public User() {
+    }
 }
