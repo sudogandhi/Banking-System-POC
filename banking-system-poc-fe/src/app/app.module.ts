06311188d359f5f23c58c05e1ebbc8031ac9aade
unknown <shivanis0703@gmail.com>
2019-09-05 16:02:23 +0530

    account details page added

--- banking-system-poc-fe/src/app/app.module.ts
+++ banking-system-poc-fe/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { AccountDetailComponent } from './account-detail/account-detail.component';
+import { AccountTransactionComponent } from './account-transaction/account-transaction.component';
+import {NgxPaginationModule} from 'ngx-pagination'; // <-- import the module
+const appRoutes: Routes=[
+{path: 'transaction',component:AccountTransactionComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountDetailComponent,
+    AccountTransactionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing:true}
+    ),
+    NgxPaginationModule//add here
   ],
   providers: [],
   bootstrap: [AppComponent]
