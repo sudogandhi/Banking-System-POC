1a4e5a4e5c7a1a63a5839bb9bd446f4b07bdac46
Guru Dayal <gurudayal37@yahoo.in>
2019-09-10 12:07:59 +0530

    C:/Program Files/Git/getAllBranches

--- banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/repositories/BranchRepository.java
+++ banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/repositories/BranchRepository.java
@@ -2,10 +2,19 @@ package com.dbs.banking.poc.bankingdemo.repositories;
 
 import com.dbs.banking.poc.bankingdemo.entities.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BranchRepository extends JpaRepository<Branch, Long> {
 
     Branch findByIfscCode(String ifscCode);
+
+    @Query("SELECT b.branchName from Branch b")
+    List<String> getBranchNames();
+
+    @Override
+    List<Branch> findAll();
 }
