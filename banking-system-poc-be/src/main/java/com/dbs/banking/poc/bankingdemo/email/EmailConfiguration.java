054bc31fed20aa43f7b12f69307bd1fa32482653
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-09-10 16:17:59 +0530

    New password is created and mailed.

--- banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/email/EmailConfiguration.java
+++ banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/email/EmailConfiguration.java
@@ -16,7 +16,7 @@ public class EmailConfiguration {
         mailSender.setPort(587);
 
         mailSender.setUsername("dbsbankpoc@gmail.com");
-        mailSender.setPassword("dbsbank123");
+        mailSender.setPassword("dbsbank");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
