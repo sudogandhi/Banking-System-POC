c104f20019d5efee539941034b917b0082386b41
Guru Dayal <gurudayal37@yahoo.in>
2019-09-11 23:35:13 +0530

    addAddress route Address

--- banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/controller/CustomerController.java
+++ banking-system-poc-be/src/main/java/com/dbs/banking/poc/bankingdemo/controller/CustomerController.java
@@ -1,13 +1,12 @@
 package com.dbs.banking.poc.bankingdemo.controller;
 
 
+import com.dbs.banking.poc.bankingdemo.co.AddressCO;
 import com.dbs.banking.poc.bankingdemo.entities.Customer;
 import com.dbs.banking.poc.bankingdemo.exceptions.UserNotExistsException;
 import com.dbs.banking.poc.bankingdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
@@ -20,5 +19,12 @@ public class CustomerController {
     public Customer userDetails() throws UserNotExistsException {
         Customer customer=customerService.getLoggedInCustomer();
         return customer;
+    }
+
+    @PutMapping(value="/addAddress")
+    public String addAddress(AddressCO addressCO) throws UserNotExistsException {
+        Customer customer=customerService.getLoggedInCustomer();
+        String response=customerService.addAddress(addressCO,customer);
+        return response;
     }
 }
